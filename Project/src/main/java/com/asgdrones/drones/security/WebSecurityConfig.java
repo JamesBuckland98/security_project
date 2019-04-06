2e55f90ec57acbeae4ff8bf761f557fd267ae4cb
c1733667 <james@james.com>
2019-04-06 12:19:01 +0100

    added HTTPS

--- Project/src/main/java/com/asgdrones/drones/security/WebSecurityConfig.java
+++ Project/src/main/java/com/asgdrones/drones/security/WebSecurityConfig.java
@@ -2,24 +2,17 @@ package com.asgdrones.drones.security;
 
 
 import com.asgdrones.drones.repositories.LoginRepoJPA;
-import com.asgdrones.drones.services.LoginService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
-import org.springframework.security.web.authentication.logout.HttpStatusReturningLogoutSuccessHandler;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
@@ -30,7 +23,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/register", "/register_address", "/register_drone", "/register_customer")
+                .antMatchers("/register", "/register_address", "/register_drone", "/register_customer", "/login")
                 .permitAll()
                 .antMatchers("/admin/**").hasAnyAuthority("admin")
                 .antMatchers("/customer/**").hasAnyAuthority("customer")
